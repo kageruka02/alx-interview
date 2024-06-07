a8cbb6a6393c0fece814c0a42755930a81fcdc68
kageruka02 <leonkageruka@gmail.com>
2024-06-07 12:10:09 +0300

    a method that determines if all the boxes can be opened.

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,14 +4,13 @@
 
 def canUnlockAll(boxes):
     "unlocking boxes"
-    push = []
-    for index in range(1, len(boxes) - 1):
-        display = boxes[0]
-        display.extend(boxes[index])
-        for number in display:
-            push.extend(boxes[number])
-        unique_list = list(set(push))
-        if index + 1 not in unique_list:
+    push = set([0])
+    display = set()
+    for index in range(0, len(boxes) - 1):
+        push.update(set(boxes[index]))
+        for i in push:
+            if i < len(boxes) and i >= 0:
+                display.update(set(boxes[i]))
+        if index + 1 not in display:
             return False
-
     return True
