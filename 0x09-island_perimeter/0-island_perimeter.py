8fc3980378cd61d21a100f68c248a25e51bced54
kageruka02 <leonkageruka@gmail.com>
2024-08-02 00:51:05 +0300

    calculating island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"returns perimeter of the island"
+
+
+def island_perimeter(grid):
+    "returns perimeter of the island"
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                # Check top
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                # Check bottom
+                if i == rows - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                # Check left
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                # Check right
+                if j == cols - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
